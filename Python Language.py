0112b4e04efd66739e69e7c22c25b6d8ef8de626
anop <81241987+mhmdnvn18@users.noreply.github.com>
2024-03-18 02:22:10 +0700

    Update Python Language.py

--- Python Language.py
+++ Python Language.py
@@ -1,11 +1,11 @@
-# Output ke layar
+# Output teks ke layar
 print("Halo, dunia!")
 
 # Variabel dan tipe data
-angka = 10
-angka_desimal = 3.14
-karakter = 'A'
-benar = True
+angka = 10  # Tipe data integer
+angka_desimal = 3.14  # Tipe data floating-point
+karakter = 'A'  # Tipe data karakter
+benar = True  # Tipe data boolean
 
 # Operasi matematika
 hasil_penjumlahan = 5 + 3
@@ -31,3 +31,27 @@ while i < 5:
 # Perulangan for
 for j in range(5):
     print("Nilai j:", j)
+
+# Tipe Data List (analog dengan Array di C++)
+arr = [1, 2, 3, 4, 5]
+
+# Menggunakan tipe data list
+print("Elemen-elemen dalam list:")
+for elem in arr:
+    print(elem)
+
+# Tipe Data Tuple (serupa dengan std::array di C++)
+floatArr = (3.14, 2.718, 1.618)
+
+# Menggunakan tipe data tuple
+print("Elemen-elemen dalam tuple:")
+for elem in floatArr:
+    print(elem)
+
+# Tipe Data List (analog dengan std::vector di C++)
+vec = [10, 20, 30]
+
+# Menggunakan tipe data list
+print("Elemen-elemen dalam list:")
+for elem in vec:
+    print(elem)
